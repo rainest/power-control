9e591818b8cfdb4fdf54dcbaf6b799758a53a938
Chris Harris <cjh@lbl.gov>
2025-06-25 15:50:14 -0400

    feat: add Postgres distributed lock (#31)
    Add an implementation of the distributed lock interface
    to the Postgres storage engine.

--- internal/storage/distlock_postgres_impl.go
+++ internal/storage/distlock_postgres_impl.go
@@ -1,44 +1,258 @@
 package storage
 
 import (
+	"context"
+	"fmt"
+	"hash/fnv"
+	"sync"
 	"time"
 
 	"github.com/jmoiron/sqlx"
 	"github.com/sirupsen/logrus"
 )
 
-type PostgresLockProvider struct {
-	logger   *logrus.Logger
-	db       *sqlx.DB
-	Duration time.Duration
+// constants used to generate unique IDs for advisory locks
+const advisoryLockNamespace = "pcs"
+const advisoryDistLockName = "distlock"
+
+// advisoryLock hold the state of a advisory lock
+type advisoryLock struct {
+	// db is the database connection pool used to acquire the advisory lock
+	db *sqlx.DB
+	// tx is the transaction that holds the advisory lock, we hold on
+	// to the transaction to ensure that we can release the lock on
+	// the same db connection. Without holding this or the connection our
+	// unlock may occur on a different connection, which would not release the lock.
+	tx *sqlx.Tx
+	// namespaceID and lockID are unique identifiers for the advisory lock
+	namespaceID int32
+	lockID      int32
+	// timeout is the duration to wait to acquire the lock
+	timeout time.Duration
+	// mutex is used to synchronize access to the lock state
+	mutex sync.Mutex
+	// logger is used for logging messages related to the advisory lock
+	logger *logrus.Logger
 }
 
-func (p *PostgresLockProvider) Init(Logger *logrus.Logger) error {
+// generateID generates a unique ID for a string using FNV-1a hash algorithm.
+func generateID(s string) int32 {
+	h := fnv.New32a()
+	h.Write([]byte(s))
+	return int32(h.Sum32())
+}
+
+// newTimedAdvisoryLock creates a new timed advisory lock with the given namespace and lock name.
+func (p *PostgresLockProvider) newTimedAdvisoryLock(namespace string, lock string, timeout time.Duration) (*advisoryLock, error) {
+	if timeout < time.Second {
+		return nil, fmt.Errorf("timeout must be >= 1 second")
+	}
+
+	// We could save these, but FNV is fast enough to generate them on the fly
+	namespaceID := generateID(namespace)
+	lockID := generateID(lock)
+
+	advisoryLock := &advisoryLock{
+		db:          p.db,
+		namespaceID: namespaceID,
+		lockID:      lockID,
+		timeout:     timeout,
+		logger:      p.logger,
+	}
+
+	return advisoryLock, nil
+}
+
+func (l *advisoryLock) acquire() error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	if l.tx != nil {
+		return fmt.Errorf("advisory lock already held")
+	}
+
+	// create a context with the lock acquisition timeout
+	timeoutCtx, timeoutCtxCancel := context.WithTimeout(context.Background(), l.timeout)
+	defer timeoutCtxCancel()
+
+	// begin a transaction for the advisory lock, the lock is scoped to the transaction
+	tx, err := l.db.BeginTxx(context.Background(), nil)
+	if err != nil {
+		return fmt.Errorf("failed to start transaction: %w", err)
+	}
+
+	// acquire the advisory lock using pg_advisory_xact_lock
+	_, err = tx.ExecContext(timeoutCtx, "SELECT pg_advisory_xact_lock($1, $2)", l.namespaceID, l.lockID)
+	if err != nil {
+		tx.Rollback()
+
+		return err
+	}
+
+	// Save the transaction in the lock state, we need it to release the lock later
+	l.tx = tx
 
 	return nil
 }
 
+// release releases the advisory lock and cleans up resources.
+func (l *advisoryLock) release() error {
+	l.mutex.Lock()
+	defer l.mutex.Unlock()
+
+	if l.tx == nil {
+		return fmt.Errorf("advisory lock not held")
+	}
+
+	// release the advisory lock by rolling back the transaction
+	err := l.tx.Rollback()
+	if err != nil {
+		return fmt.Errorf("failed to release advisory lock: %w", err)
+	}
+
+	// reset the lock state
+	l.tx = nil
+
+	return nil
+}
+
+// PostgresLockProvider implements a distributed lock provider using PostgreSQL's advisory locks.
+type PostgresLockProvider struct {
+	// Config holds the postgres configuration
+	Config PostgresConfig
+	// logger is used for logging messages
+	logger *logrus.Logger
+	// db is the database connection used to acquire advisory locks
+	db *sqlx.DB
+	// lock is the current advisory lock held by this provider
+	lock *advisoryLock
+	// sync.Mutex is used to synchronize access to the lock state
+	mutex sync.Mutex
+}
+
+// Init initializes the PostgresLockProvider with a logger.
+func (p *PostgresLockProvider) Init(logger *logrus.Logger) error {
+	if logger == nil {
+		p.logger = logrus.New()
+	} else {
+		p.logger = logger
+	}
+
+	db, err := OpenDB(p.Config, logger)
+	if err != nil {
+		return fmt.Errorf("failed to open database connection: %w", err)
+	}
+
+	p.db = sqlx.NewDb(db, "postgres")
+
+	return nil
+}
+
+// Ping checks the connection to the PostgreSQL database.
 func (p *PostgresLockProvider) Ping() error {
+	if p.db == nil {
+		return fmt.Errorf("instance closed or not initialized")
+	}
+
+	if err := p.db.Ping(); err != nil {
+		return fmt.Errorf("Ping failed: %v", err)
+	}
+
+	return nil
+}
+
+// Close closes the database connection used by the PostgresLockProvider.
+func (p *PostgresLockProvider) Close() (err error) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	if p.db == nil {
+		return fmt.Errorf("instance closed or not initialized")
+	}
+
+	// if there is an active lock, release it before closing the database connection
+	if p.lock != nil {
+		err = p.lock.release()
+		if err != nil {
+			return fmt.Errorf("failed to release lock: %v", err)
+		}
+		p.lock = nil
+	}
+
+	// close the database connection
+	if p.db != nil {
+		if err := p.db.Close(); err != nil {
+			return fmt.Errorf("Close failed: %v", err)
+		}
+	}
+
+	p.db = nil
+	p.lock = nil
+
 	return nil
 }
 
 func (p *PostgresLockProvider) DistributedTimedLock(maxLockTime time.Duration) error {
-	p.Duration = maxLockTime
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	if p.db == nil {
+		return fmt.Errorf("instance closed or not initialized")
+	}
+
+	if maxLockTime < time.Second {
+		return fmt.Errorf("lock duration request invalid (%s) -- must be >= 1sec.",
+			maxLockTime.String())
+	}
+
+	if p.lock != nil {
+		return fmt.Errorf("lock already held")
+	}
+
+	// create a new timed advisory lock
+	lock, err := p.newTimedAdvisoryLock(advisoryLockNamespace, advisoryDistLockName, maxLockTime)
+	if err != nil {
+		return err
+	}
+
+	// acquire the advisory lock
+	if err := lock.acquire(); err != nil {
+		return err
+	}
+
+	p.lock = lock
+
 	return nil
 }
 
 func (p *PostgresLockProvider) Unlock() error {
-	p.Duration = 0
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	if p.db == nil {
+		return fmt.Errorf("instance closed or not initialized")
+	}
+
+	if p.lock == nil {
+		return fmt.Errorf("no lock held to release")
+	}
+
+	err := p.lock.release()
+	if err != nil {
+		return fmt.Errorf("failed to release distributed lock: %v", err)
+	}
+	p.lock = nil
+
 	return nil
 }
 
 func (p *PostgresLockProvider) GetDuration() time.Duration {
-	return p.Duration
-}
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
 
-func (p *PostgresLockProvider) Close() error {
-	if p.db != nil {
-		return p.db.Close()
+	if p.lock == nil {
+		return 0
 	}
-	return nil
+
+	return p.lock.timeout
 }
--- internal/storage/distlock_test.go
+++ internal/storage/distlock_test.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"sync"
+	"sync/atomic"
 	"testing"
 	"time"
 
+	_ "github.com/lib/pq"
 	"github.com/sirupsen/logrus"
 	"github.com/stretchr/testify/require"
 	"github.com/testcontainers/testcontainers-go"
@@ -19,22 +22,23 @@ const (
 	TEST_FAIL_CODE = 136
 )
 
+const (
+	postgresDBName   = "pcsdb"
+	postgresUsername = "pcsuser"
+	postgresPassword = "mysecretpassword"
+)
+
 var (
-	storageProvider  StorageProvider
 	distLockProvider DistributedLockProvider
 )
 
 func startPostgresContainer() (testcontainers.Container, error) {
-	dbName := "pcsdb"
-	username := "pcsuser"
-	password := "nothingtoseehere"
-
 	ctx := context.Background()
 	ctr, err := testpostgres.Run(ctx,
 		"postgres:16-alpine",
-		testpostgres.WithDatabase(dbName),
-		testpostgres.WithUsername(username),
-		testpostgres.WithPassword(password),
+		testpostgres.WithDatabase(postgresDBName),
+		testpostgres.WithUsername(postgresUsername),
+		testpostgres.WithPassword(postgresPassword),
 		testcontainers.WithWaitStrategy(
 			wait.ForLog("database system is ready to accept connections").
 				WithOccurrence(2).
@@ -68,24 +72,6 @@ func startStorageBackendContainer() (testcontainers.Container, error) {
 	}
 }
 
-func createStorageProvider() (StorageProvider, error) {
-	storage := os.Getenv("STORAGE")
-	var provider StorageProvider
-	switch storage {
-	case "MEMORY":
-		provider = &MEMStorage{}
-	case "ETCD":
-		provider = &ETCDStorage{}
-	case "POSTGRES":
-		// TODO Setup postgres config here
-		provider = &PostgresStorage{}
-	default:
-		return nil, fmt.Errorf("Unknown storage type: %s", storage)
-	}
-
-	return provider, nil
-}
-
 func createDistLockProvider() (DistributedLockProvider, error) {
 	storage := os.Getenv("STORAGE")
 	var provider DistributedLockProvider
@@ -95,7 +81,14 @@ func createDistLockProvider() (DistributedLockProvider, error) {
 	case "ETCD":
 		provider = &ETCDLockProvider{}
 	case "POSTGRES":
-		provider = &PostgresLockProvider{}
+		postgresConfig := DefaultPostgresConfig()
+		postgresConfig.Host = "localhost"
+		postgresConfig.DBName = postgresDBName
+		postgresConfig.User = postgresUsername
+		postgresConfig.Password = postgresPassword
+		postgresConfig.Insecure = true
+
+		provider = &PostgresLockProvider{Config: postgresConfig}
 	default:
 		return nil, fmt.Errorf("Unknown storage type: %s", storage)
 	}
@@ -131,25 +124,13 @@ func TestMain(m *testing.M) {
 	//	fmt.Printf("Error starting storage backend container: %v\n", err)
 	//	os.Exit(TEST_FAIL_CODE)
 	//}
-
 	var err error
-	storageProvider, err = createStorageProvider()
-	if err != nil {
-		fmt.Printf("Error creating storage provider: %v\n", err)
-		os.Exit(TEST_FAIL_CODE)
-	}
-
 	distLockProvider, err = createDistLockProvider()
 	if err != nil {
 		fmt.Printf("Error creating distributed lock provider: %v\n", err)
 		os.Exit(TEST_FAIL_CODE)
 	}
-	// Initialize the storage provider
-	err = storageProvider.Init(nil)
-	if err != nil {
-		fmt.Printf("Error initializing storage provider: %v\n", err)
-		os.Exit(TEST_FAIL_CODE)
-	}
+
 	// Initialize the distributed lock provider
 	err = distLockProvider.Init(logrus.New())
 	if err != nil {
@@ -167,20 +148,218 @@ func TestPing(t *testing.T) {
 	require.NoError(t, err, "DistLock Ping() failed")
 }
 func TestDistributedLock(t *testing.T) {
-	lockDur := 10 * time.Second
-	err := distLockProvider.DistributedTimedLock(lockDur)
+	timeout := 10 * time.Second
+	err := distLockProvider.DistributedTimedLock(timeout)
 	require.NoError(t, err, "DistributedTimedLock() failed")
 
 	time.Sleep(1 * time.Second)
-	if distLockProvider.GetDuration() != lockDur {
-		t.Errorf("Lock duration readout failed, expecting %s, got %s",
-			lockDur.String(), distLockProvider.GetDuration().String())
-	}
+	require.Equal(t, distLockProvider.GetDuration(), timeout,
+		"Lock duration readout failed, expecting %s, got %s",
+		timeout.String(), distLockProvider.GetDuration().String())
+
 	err = distLockProvider.Unlock()
 	require.NoErrorf(t, err, "Error releasing timed lock (outer): %v", err)
 
-	if distLockProvider.GetDuration() != 0 {
-		t.Errorf("Lock duration readout failed, expecting 0s, got %s",
-			distLockProvider.GetDuration().String())
+	require.Equal(t, distLockProvider.GetDuration(), 0*time.Second,
+		"Lock duration readout failed, expecting 0s, got %s",
+		distLockProvider.GetDuration().String())
+}
+
+func TestDistrubutedLockAlreadyAcquired(t *testing.T) {
+	err := distLockProvider.DistributedTimedLock(5 * time.Second)
+	require.NoError(t, err, "DistributedTimedLock() failed")
+
+	// Attempt to acquire the lock again
+	err = distLockProvider.DistributedTimedLock(1 * time.Second)
+	require.Error(t, err, "Expected error when trying to acquire lock again, but got none")
+}
+
+func TestDistributedLockTimeout(t *testing.T) {
+	err := distLockProvider.DistributedTimedLock(4 * time.Second)
+	require.NoError(t, err, "DistributedTimedLock() failed")
+
+	// Attempt to acquire the lock again with a different provider
+	distLockProvider2, err := createDistLockProvider()
+	require.NoError(t, err, "Error creating second distributed lock provider")
+
+	// Initialize the second distributed lock provider
+	err = distLockProvider2.Init(logrus.New())
+	require.NoError(t, err, "Error initializing second distributed lock provider")
+
+	// This should timeout
+	err = distLockProvider2.DistributedTimedLock(1 * time.Second)
+	if err == nil {
+		t.Error("Expected error when trying to acquire lock again, but got none")
 	}
+
+	// Now unlock the first provider
+	err = distLockProvider.Unlock()
+	require.NoError(t, err, "Unlock() failed")
+
+	// Now try to acquire the lock again after it should have timed out
+	err = distLockProvider2.DistributedTimedLock(1 * time.Second)
+	require.NoError(t, err, "Expected to acquire lock after timeout, but got error")
+
+	// Clean up the second provider
+	err = distLockProvider2.Close()
+	require.NoError(t, err, "Close() failed")
+}
+
+func TestDistributedLockUnlock(t *testing.T) {
+	lockDur := 10 * time.Second
+
+	err := distLockProvider.DistributedTimedLock(lockDur)
+	require.NoError(t, err, "DistributedTimedLock() failed")
+
+	// Attempt to release the lock
+	err = distLockProvider.Unlock()
+	require.NoError(t, err, "Unlock() failed")
+
+	// We should be able to acquire the lock again
+	err = distLockProvider.DistributedTimedLock(lockDur)
+	require.NoError(t, err, "Expected to acquire lock after unlock, but got error")
+
+	err = distLockProvider.Unlock()
+	require.NoError(t, err, "Unlock() failed")
+
+}
+
+// TestDistributedLockClose tests that the Close method of the DistributedLockProvider will release
+// the lock if it is held.
+func TestDistributedLockClose(t *testing.T) {
+	// Create a new distributed lock provider as Close will render
+	// fixture distLockProvider unusable.
+	lockProvider, err := createDistLockProvider()
+	require.NoError(t, err, "Error creating distributed lock provider")
+
+	err = lockProvider.Init(logrus.New())
+	require.NoError(t, err, "Error initializing distributed lock provider")
+
+	err = lockProvider.DistributedTimedLock(60 * time.Second)
+	require.NoError(t, err, "Expected to acquire lock before Close(), but got error")
+
+	err = lockProvider.Close()
+	require.NoError(t, err, "Close() failed")
+
+	// After closing the lock provider, we should not be able call any methods on it
+	err = lockProvider.Ping()
+	require.Error(t, err, "Expected error when calling Ping() on closed lock provider, but got none")
+
+	err = lockProvider.DistributedTimedLock(5 * time.Second)
+	require.Error(t, err, "Expected error when trying to acquire lock on closed lock provider, but got none")
+
+	err = lockProvider.Unlock()
+	require.Error(t, err, "Expected error when trying to unlock on closed lock provider, but got none")
+
+	err = lockProvider.Close()
+	require.Error(t, err, "Expected error when trying to close already closed lock provider, but got none")
+
+	// Ensure that we can now acquire the lock again
+	lockProvider2, err := createDistLockProvider()
+	require.NoError(t, err, "Error creating second distributed lock provider")
+
+	// Initialize the second distributed lock provider
+	err = lockProvider2.Init(logrus.New())
+	require.NoError(t, err, "Error initializing second distributed lock provider")
+
+	err = lockProvider2.DistributedTimedLock(5 * time.Second)
+	require.NoError(t, err, "Expected to acquire lock after Close(), but got error")
+
+	// Clean up by unlocking the second provider
+	err = lockProvider2.Close()
+	require.NoError(t, err, "Close() failed")
+}
+
+type lockMonitor struct {
+	current atomic.Int32
+	max     atomic.Int32
+}
+
+func (m *lockMonitor) increment() {
+	current := m.current.Add(1)
+	// Update max if current is greater using a compare-and-swap loop
+	for {
+		max := m.max.Load()
+		if current <= max || m.max.CompareAndSwap(max, current) {
+			break
+		}
+	}
+}
+
+func (m *lockMonitor) decrement() {
+	m.current.Add(-1) // Decrement current by 1
+}
+
+func aquireLock(lockAttemptResult chan error, waitGroup *sync.WaitGroup, log *logrus.Logger, monitor *lockMonitor) {
+	defer waitGroup.Done()
+
+	dlp, err := createDistLockProvider()
+	if err != nil {
+		lockAttemptResult <- err
+		return
+	}
+
+	dlp.Init(log)
+
+	err = dlp.DistributedTimedLock(20 * time.Second)
+
+	if err != nil {
+		lockAttemptResult <- err
+		return
+	}
+
+	monitor.increment()
+
+	// Do some "work" while holding the lock
+	time.Sleep(150 * time.Millisecond)
+
+	monitor.decrement()
+
+	err = dlp.Unlock()
+	if err != nil {
+		lockAttemptResult <- err
+		return
+	}
+
+	lockAttemptResult <- nil
+}
+
+func TestDistributedLockGoRoutineRace(t *testing.T) {
+	numGoroutines := 50
+	// Channels to communicate results from goroutines
+	lockAttemptResults := make(chan error, numGoroutines)
+	// Wait group to wait for all goroutines to finish
+	var wg sync.WaitGroup
+	// Monitor to track successful lock acquisitions
+	var monitor lockMonitor
+
+	wg.Add(numGoroutines)
+
+	// Create a logger for the goroutines
+	log := logrus.New()
+	// Attempt to acquire the lock in multiple goroutines
+	for i := 0; i < numGoroutines; i++ {
+		go aquireLock(lockAttemptResults, &wg, log, &monitor)
+	}
+
+	wg.Wait()
+	close(lockAttemptResults)
+
+	// Check results from the goroutines, only one should successfully acquire the lock
+	// and the rest should cancelled wait on the lock.
+	successfullLocks := 0
+
+	for i := 0; i < numGoroutines; i++ {
+		err := <-lockAttemptResults
+		if err == nil {
+			successfullLocks++
+		} else {
+			t.Errorf("Goroutine %d failed with error: %v\n", i, err)
+		}
+	}
+
+	// We should never have more than one goroutine holding the lock at a time
+	require.Equal(t, monitor.max.Load(), int32(1), "Expected only one goroutine to hold the lock at a time, but got %d", monitor.max.Load())
+	// Everyone should eventually succeed in acquiring the lock, but only one at a time
+	require.Equal(t, numGoroutines, successfullLocks, "Expected exactly %d successful lock acquisitions, got %d", numGoroutines, successfullLocks)
 }
