c20ba37f0976f9a7135e512e377ec310bbef96b8
Travis Raines <571832+rainest@users.noreply.github.com>
2025-08-17 19:10:04 -0700

    chore: hardcode transition dispatch hostname

--- internal/domain/transitions.go
+++ internal/domain/transitions.go
@@ -784,7 +784,7 @@ func doTransition(transitionID uuid.UUID) {
 			comp.Task.Operation = powerActionOp
 			trsTaskMap[trsTaskList[trsTaskIdx].GetID()] = comp
 			trsTaskList[trsTaskIdx].CPolicy.Retry.Retries = 3
-			trsTaskList[trsTaskIdx].Request, _ = http.NewRequest("POST", "https://"+comp.HSMData.RfFQDN+comp.HSMData.PowerActionURI, bytes.NewBuffer([]byte(payload)))
+			trsTaskList[trsTaskIdx].Request, _ = http.NewRequest("POST", "http://"+"default-fred-virtbmc.kubevirtbmc-system"+comp.HSMData.PowerActionURI, bytes.NewBuffer([]byte(payload)))
 			trsTaskList[trsTaskIdx].Request.Header.Set("Content-Type", "application/json")
 			trsTaskList[trsTaskIdx].Request.Header.Add("HMS-Service", GLOB.BaseTRSTask.ServiceName)
 			// Vault enabled?
